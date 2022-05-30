044f391f5a41c9e686508cf40dfdf9752703b7f4
Miguel <miguelsalvador.rguez@gmail.com>
2022-05-30 23:21:34 +0200

    Añadidos cambios

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 
 
-var sentence = document.getElementById("commands").value;
-function splitString(sentence){
-    var words = sentence.split(" ");
-    console.log(words[0]);
-    console.log(words[1]);
+function splitString(orders){
+    let words = orders.split(" ");
+    return words;
 }
+
+
 
 const button = document.getElementById("do");
 button.onclick = function (){
-    splitString(sentence);
-} 
+    let sentence = document.getElementById("commands").value;
+    let order = splitString(sentence);
+    let checked = verbs.has (order[0]);
+    if ((!checked)){
+        console.log("Solo puedes utilizar los verbos coger, usar, ir, mirar, hablar")
+    }
+    
+}
+
+
+
+const verbs = new Map();
+verbs.set("coger");
+verbs.set("usar");
+verbs.set("ir");
+verbs.set("mirar");
+verbs.set("hablar");
+
+
+
