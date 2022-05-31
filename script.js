c21c1035099bcbfc7ef0b071e435bb675ff28c1c
Miguel <miguelsalvador.rguez@gmail.com>
2022-05-31 15:29:04 +0200

    JS updated

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+const game = actions();
+const mapa = createMap();
+let player ={position: [0,0]} 
 
+const button = document.getElementById("do");
+button.onclick = executeCommands(); 
+   
 
 function splitString(orders){
     let words = orders.split(" ");
     return words;
 }
 
+function actions(){
 
+    const verbs = new Map();
+    verbs.set("coger");
+    verbs.set("usar");
+    verbs.set("ir");
+    verbs.set("mirar");
+    verbs.set("hablar");
+    return verbs;
+}
 
-const button = document.getElementById("do");
-button.onclick = function (){
+function executeCommands(){
     let sentence = document.getElementById("commands").value;
     let order = splitString(sentence);
     let checked = verbs.has (order[0]);
@@ -18,14 +32,32 @@ button.onclick = function (){
     
 }
 
+function createMap(){
+    let Map = new Array(4);
+    for (let i = 0; i < 10; i++){
+        Map[i]= new Array(4)
+    }
+
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+    Map[0][0]=
+}
 
 
-const verbs = new Map();
-verbs.set("coger");
-verbs.set("usar");
-verbs.set("ir");
-verbs.set("mirar");
-verbs.set("hablar");
+
 
 
 
