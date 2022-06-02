3ac51c54911ba9d4c87853f7ed4654a80e7e4abc
Miguel Angel Salvador <miguelangelsalvador@Miguels-MacBook-Pro.local>
2022-06-02 12:04:41 +0200

    Pequeños cambios en JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("perro");
 const game = actions();
 const mapa = createMap();
 let player ={position: [0,0]} 
@@ -38,22 +39,23 @@ function createMap(){
         Map[i]= new Array(4)
     }
 
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
-    Map[0][0]=
+    Map[0][0]= {description:"Entrada de la cueva", exits:["S"]};
+    Map[0][1]= {description:"Habitación de los niños. Duermen 30 niños en camas de paja. Solo uno está despierto", exits:["S"]};
+    Map[0][2]= {description:"Habitación de los adultos. Duermen 10 adultos en camas de paja", exits:["S"]};
+    Map[0][3]= {description:"Habitación del jefe de la tribu.", exits:[""]};
+    Map[1][0]= {description:"Percher", exits:["N", "E", "S"]};
+    Map[1][1]= {description:"", exits:["W", "E"]};
+    Map[1][2]= {description:"", exits:["W", "E"]};
+    Map[1][3]= {description:"", exits:["S", "W"]};
+    Map[2][0]= {description:"", exits:["N", "E", "S"]};
+    Map[2][1]= {description:"", exits:["E", "S", "W"]};
+    Map[2][2]= {description:"", exits:["S", "W"]};
+    Map[2][3]= {description:"", exits:["N", "S"]};
+    Map[3][0]= {description:"", exits:["N"]};
+    Map[3][1]= {description:"", exits:["N"]};
+    Map[3][2]= {description:"", exits:["N"]};
+    Map[3][3]= {description:"", exits:["N"]};
+
 }
 
 
